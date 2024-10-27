26f08241e2d430ff1dcdd35edffc1e8f4fb0cb54
Furydrack <marius.boulandet@gmail.com>
2024-10-27 12:51:58 +0100

    UI detritu info etc

--- Assets/Scripts/GameManagement.cs
+++ Assets/Scripts/GameManagement.cs
@@ -14,8 +14,14 @@ public class GameManagement : MonoBehaviour
     public int movements = 0; // Player movements count 
     public Dictionary<string,(int,int)> detritusFrequences = new Dictionary<string,(int,int)>
     {
-        {"SO",(1,5)}, {"SP",(1,4)},{"MO",(0,1)},{"MP",(0,1)},{"GO",(0,1)},{"GP",(0,1)}
+        // Frequences : "String" item appears Item1 times every Item2 movements
+        {"SO",(1,5)}, {"SP",(1,4)},{"MO",(0,1)},{"MP",(0,1)},{"GO",(0,1)},{"GP",(0,1)},
+
+        // Special apparition
+        {"BEBER",(3,10)} // Beber has 3% chance to appears every 10 movements
+
     };
+
     public DetrituData[] detritus;
     public (float,int) piecesSpawner; // Probability A for a piece to spawn every B movements
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -61,9 +67,17 @@ public class GameManagement : MonoBehaviour
 
     private void AddDetritus(string type)
     {
-        for(int i =0; i < detritusFrequences[type].Item1; i++)
+        if(type=="BEBER")
         {
-            MapController.instance.detritus.GetComponent<DetritusApparition>().Apparition(type);
+            if(detritusFrequences[type].Item1 > Random.Range(0,100))
+                MapController.instance.detritus.GetComponent<DetritusApparition>().Apparition(type);
+        }
+        else
+        {
+            for(int i =0; i < detritusFrequences[type].Item1; i++)
+            {
+                MapController.instance.detritus.GetComponent<DetritusApparition>().Apparition(type);
+            }
         }
     }
 }
