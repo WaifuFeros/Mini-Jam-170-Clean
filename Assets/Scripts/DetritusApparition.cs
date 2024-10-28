aae21dff7827117a5a4e5e18596bab2620c1c637
WaifuFeros <payet.pep@gmail.com>
2024-10-28 04:36:29 +0100

    build qui marche

--- Assets/Scripts/DetritusApparition.cs
+++ Assets/Scripts/DetritusApparition.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.LightTransport;
+//using UnityEngine.LightTransport;
 using UnityEngine.Tilemaps;
 
 public class DetritusApparition : MonoBehaviour
